384c28cb72be161c6d1f72802641d133e1b883e8
farvastra <farvastra@gmail.com>
2025-02-16 17:26:19 +0100

    Update CORS for deployed frontend

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {g
+  if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
