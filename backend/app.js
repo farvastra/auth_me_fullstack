f4bc2ac10a08146f407a661d95d8ecd1314f2e59
farvastra <farvastra@gmail.com>
2025-02-20 14:55:52 +0100

    removed  path

--- backend/app.js
+++ backend/app.js
@@ -32,12 +32,6 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.use(
   cookieSession({
     name: "session",
